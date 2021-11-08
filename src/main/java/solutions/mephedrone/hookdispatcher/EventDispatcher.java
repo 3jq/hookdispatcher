e926429fbc2c4d9ae1f5e8ff48e28d5b5116845c
3jq <fuckyouthinkimboogieman@protonmail.com>
2021-11-08 19:00:52 +0300

    This is much faster than older dispatcher thing

--- src/main/java/solutions/mephedrone/hookdispatcher/EventDispatcher.java
+++ src/main/java/solutions/mephedrone/hookdispatcher/EventDispatcher.java
@@ -5,35 +5,40 @@ import org.jetbrains.annotations.NotNull;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author yoursleep
  * @since 08 November 2021
  */
 public class EventDispatcher {
-    private final List<Object> registeredObjects;
+    private final Map<Object, List<Method>> registeredObjects;
 
-    public EventDispatcher() { registeredObjects = new ArrayList<>(); }
+    public EventDispatcher() { registeredObjects = new HashMap<>(); }
 
-    public void register(@NotNull Object object) { if (!registeredObjects.contains(object)) registeredObjects.add(object); }
+    public void register(@NotNull Object object) {
+        if (registeredObjects.containsKey(object)) return;
+        final List<Method> methods = new ArrayList<>();
+        for (Method method : object.getClass().getMethods()) if (method.getAnnotation(Subscribe.class) != null && method.getParameterCount() == 1) methods.add(method);
+        registeredObjects.put(object, methods);
+    }
 
-    public void unregister(@NotNull Object object) { if (registeredObjects.contains(object)) registeredObjects.remove(object); }
+    public void unregister(@NotNull Object object) {
+        registeredObjects.remove(object);
+    }
 
     public void dispatch(@NotNull Object event) {
-        for (Object object : registeredObjects) {
-            for (Method method : object.getClass().getMethods()) {
-                if (
-                        method.getAnnotation(Subscribe.class) != null
-                        && method.getParameterCount() == 1
-                        && method.getParameterTypes()[0].equals(event.getClass())
-                ) {
-                    try {
-                        method.invoke(object, event);
-                    } catch (InvocationTargetException | IllegalAccessException e) {
-                        System.out.println("Failed to dispatch event " + event.getClass().getSimpleName() + "!");
-                        e.printStackTrace();
-                    }
+        for (Map.Entry<Object, List<Method>> entry : registeredObjects.entrySet()) {
+            for (Method method : entry.getValue()) {
+                if (!method.getParameterTypes()[0].equals(event.getClass())) continue;
+
+                try {
+                    method.invoke(entry.getKey(), event);
+                } catch (InvocationTargetException | IllegalAccessException e) {
+                    System.out.println("Failed to dispatch event " + event.getClass().getSimpleName() + "!");
+                    e.printStackTrace();
                 }
             }
         }
